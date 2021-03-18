b32f6a4e6c343aa2092d56b1d93830ed13590617
tilman <tilman.grueger@web.de>
2021-03-18 16:22:38 +0100

    fdwqa

--- PyFinra/earningsinfo.py
+++ PyFinra/earningsinfo.py
@@ -18,28 +18,33 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
-df = pd.read_csv(io.StringIO(response.text), header=1)
+df_test = pd.read_csv(io.StringIO(response.text), header=1)
+
+indexcol = df_test["Fiscal year ends in December. USD in millions except per share data."].to_list()
+print(indexcol)
+df = pd.read_csv(io.StringIO(response.text), header=1, index_col=indexcol)
 print(df)
 # print(pd.DataFrame.to_json(df))
-print(df.at[,"2020-12"])
+# print(df)
 # print(df.to_dict())
 
 
-'''kurzfassung'''
+# '''kurzfassung'''
 
-url = "http://finra-markets.morningstar.com/getQtComponent.jsp?pagename=17&path=http%3A%2F%2Ffinancials.morningstar.com%2Fquote%2Ffinancial.html%3Ft%3DXNAS%3ATSLA%26culture%3Den-US%26r%3D344290&_=1616074869410"
+# url = "http://finra-markets.morningstar.com/getQtComponent.jsp?pagename=17&path=http%3A%2F%2Ffinancials.morningstar.com%2Fquote%2Ffinancial.html%3Ft%3DXNAS%3ATSLA%26culture%3Den-US%26r%3D344290&_=1616074869410"
 
-payload={}
-headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
-  'Accept': '*/*',
-  'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Connection': 'keep-alive',
-  'Referer': 'http://finra-markets.morningstar.com/qt_financial_quote.htm?reportType=fq&t=XNAS:TSLA&region=USA&client=FINRA&theme=default&qt_version=finrarelease&tickerType=ST&detail=true&version=2&culture=en-US&qs_qt_module=3.1311',
-  'Cookie': '_gcl_au=1.1.1913597115.1612797518; _ga=GA1.2.1914022575.1612797519; _fbp=fb.1.1612797519515.1481886258; ELOQUA=GUID=2BEA7B0D5FE746CFA7D2EE4F9C0ECB86; __utma=93401610.1914022575.1612797519.1616059921.1616074805.14; __utmz=93401610.1616074805.14.8.utmcsr=finra-markets.morningstar.com|utmccn=(referral)|utmcmd=referral|utmcct=/MarketData/EquityOptions/detail.jsp; __utma=153686052.1914022575.1612797519.1616059921.1616074805.14; __utmz=153686052.1616074805.14.14.utmcsr=finra-markets.morningstar.com|utmccn=(referral)|utmcmd=referral|utmcct=/MarketData/EquityOptions/detail.jsp; _hp2_id.3604294647=%7B%22userId%22%3A%226480641378265027%22%2C%22pageviewId%22%3A%224532158810670265%22%2C%22sessionId%22%3A%227897848520037163%22%2C%22identity%22%3Anull%2C%22trackerVersion%22%3A%224.0%22%7D; __cfduid=dad353a1af9c835e90113482228a4ef221615734596; qs_wsid=85C2903C583A59422373CD63A0C8F7E9; __cfruid=139e19a14b399643ebd1727c26636667522a27e2-1616056709; SessionID=85C2903C583A59422373CD63A0C8F7E9; UsrID=41151; UsrName=FINRA.QSAPIDEF@morningstar.com; __utmc=93401610; srtqs=eyJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.mHdD03fiH7GfkyAjrGAudPqFJAJpzauDnOmt0r3CJS9Yq_mpOFhACg0IUc_D9WpcG_w7-Fiz1kAWJ1IdHp8p1n5O0AEBbGcsH8Q42reM6jXKTbnmLPfYk4CZpLFRhR1rSlBR94E0SEAmkoALTnooe2CaWF-SJRZpbzXswH6Jh9A.m0XbmvaFeQ3_t9ZT.Ecvx07jJaC8vh_go9iRp_uoDah5Qpe7mRCumSi5gfDpL8GAuoMCqAHK_pu3QjrEsXkrdXwjyo-qZDoWqBBgPNPddYOQuZeDuQ67r7fv4JUuTc6YiATG6MWSO-PNIQIMtFXc_KPLpmmbDlH6GHZsnNpndJWcpHuUiyY9BRC9G0oOQF4nJ9oG2Hp5kEN8h8yqL40Typdy3Smj3g1TKusKh3X5Vtw.TKqRiBwkRKgFVdwWQc7PMQ; srtqsv2=eyJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.MySJuNwcWRruYiLSPq9qH6II9TNdrDhVblz0HqPJSQ9GtQ1D-Aqezp98HPCuZyJOrQlRTl805y6_EU_xW2XkJAPRjGijQ8KJqAbVQE3lotGIBncB-zuwRb0KmsgpGvgwZz1JrLyJjFJ87pc5qVy-EdAxzy-ATe8mLUiF-SDVK-w.ilTUr1ZUFBTnuQo0.ErVyP3gxCJPzRkHJupgN9iPomXCgFql2lnYEZ6Pf9KIHhslK_Zjk3flx6pXX_-93CPcSUnMhyRblcUKJyHqG7u8NqOyoAdxhwlmwZza6-tzH2pknRM3zeroPlxjXPax69nowkNOQRzGiEP5Hv3GyWcKLS72z_8Hwa0nMekJytD3uEeIW-_ATDo6xteLotrISz1x7YNRHMHwn-H31Gs3enHEZjcPlp8yetamxrvNIg4Y0.Q6fq13_Mi2SA0M1QOs6wJw; __utmc=153686052; qs_api_version=finrarelease; theme=default; APIclientId=default; Instid=FINRA; __utmb=93401610.2.10.1616074805; __utmt=1; __utmb=153686052.2.10.1616074805; __utmt_MM=1; __cfduid=d32a4f5194dfaa0699541ba4cd95a8ee61614448494'
-}
+# payload={}
+# headers = {
+#   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+#   'Accept': '*/*',
+#   'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+#   'X-Requested-With': 'XMLHttpRequest',
+#   'Connection': 'keep-alive',
+#   'Referer': 'http://finra-markets.morningstar.com/qt_financial_quote.htm?reportType=fq&t=XNAS:TSLA&region=USA&client=FINRA&theme=default&qt_version=finrarelease&tickerType=ST&detail=true&version=2&culture=en-US&qs_qt_module=3.1311',
+#   'Cookie': '_gcl_au=1.1.1913597115.1612797518; _ga=GA1.2.1914022575.1612797519; _fbp=fb.1.1612797519515.1481886258; ELOQUA=GUID=2BEA7B0D5FE746CFA7D2EE4F9C0ECB86; __utma=93401610.1914022575.1612797519.1616059921.1616074805.14; __utmz=93401610.1616074805.14.8.utmcsr=finra-markets.morningstar.com|utmccn=(referral)|utmcmd=referral|utmcct=/MarketData/EquityOptions/detail.jsp; __utma=153686052.1914022575.1612797519.1616059921.1616074805.14; __utmz=153686052.1616074805.14.14.utmcsr=finra-markets.morningstar.com|utmccn=(referral)|utmcmd=referral|utmcct=/MarketData/EquityOptions/detail.jsp; _hp2_id.3604294647=%7B%22userId%22%3A%226480641378265027%22%2C%22pageviewId%22%3A%224532158810670265%22%2C%22sessionId%22%3A%227897848520037163%22%2C%22identity%22%3Anull%2C%22trackerVersion%22%3A%224.0%22%7D; __cfduid=dad353a1af9c835e90113482228a4ef221615734596; qs_wsid=85C2903C583A59422373CD63A0C8F7E9; __cfruid=139e19a14b399643ebd1727c26636667522a27e2-1616056709; SessionID=85C2903C583A59422373CD63A0C8F7E9; UsrID=41151; UsrName=FINRA.QSAPIDEF@morningstar.com; __utmc=93401610; srtqs=eyJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.mHdD03fiH7GfkyAjrGAudPqFJAJpzauDnOmt0r3CJS9Yq_mpOFhACg0IUc_D9WpcG_w7-Fiz1kAWJ1IdHp8p1n5O0AEBbGcsH8Q42reM6jXKTbnmLPfYk4CZpLFRhR1rSlBR94E0SEAmkoALTnooe2CaWF-SJRZpbzXswH6Jh9A.m0XbmvaFeQ3_t9ZT.Ecvx07jJaC8vh_go9iRp_uoDah5Qpe7mRCumSi5gfDpL8GAuoMCqAHK_pu3QjrEsXkrdXwjyo-qZDoWqBBgPNPddYOQuZeDuQ67r7fv4JUuTc6YiATG6MWSO-PNIQIMtFXc_KPLpmmbDlH6GHZsnNpndJWcpHuUiyY9BRC9G0oOQF4nJ9oG2Hp5kEN8h8yqL40Typdy3Smj3g1TKusKh3X5Vtw.TKqRiBwkRKgFVdwWQc7PMQ; srtqsv2=eyJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.MySJuNwcWRruYiLSPq9qH6II9TNdrDhVblz0HqPJSQ9GtQ1D-Aqezp98HPCuZyJOrQlRTl805y6_EU_xW2XkJAPRjGijQ8KJqAbVQE3lotGIBncB-zuwRb0KmsgpGvgwZz1JrLyJjFJ87pc5qVy-EdAxzy-ATe8mLUiF-SDVK-w.ilTUr1ZUFBTnuQo0.ErVyP3gxCJPzRkHJupgN9iPomXCgFql2lnYEZ6Pf9KIHhslK_Zjk3flx6pXX_-93CPcSUnMhyRblcUKJyHqG7u8NqOyoAdxhwlmwZza6-tzH2pknRM3zeroPlxjXPax69nowkNOQRzGiEP5Hv3GyWcKLS72z_8Hwa0nMekJytD3uEeIW-_ATDo6xteLotrISz1x7YNRHMHwn-H31Gs3enHEZjcPlp8yetamxrvNIg4Y0.Q6fq13_Mi2SA0M1QOs6wJw; __utmc=153686052; qs_api_version=finrarelease; theme=default; APIclientId=default; Instid=FINRA; __utmb=93401610.2.10.1616074805; __utmt=1; __utmb=153686052.2.10.1616074805; __utmt_MM=1; __cfduid=d32a4f5194dfaa0699541ba4cd95a8ee61614448494'
+# }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+# print(response.text)
+
